5484b9b302f105deaeab905cba524bc5742073c9
ChalecoAmarillo <150099016+ChalecoAmarillo@users.noreply.github.com>
2024-02-23 19:27:14 -0300

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,3 +1,3 @@
 var scriptCode = `
-  document.write('<script type="text/javascript" src="//wideaplentyinsurance.com/6f/b4/ae/6fb4ae4ccbf4dd54c7f8d6ce47dd7353.js"></script>');
+  document.write('<script src="//tv.apurofutbol.net/miads.js?v:1"></script> ');
 `;
