353eea066bad9d308154b422ae4c0e31c30f8401
ChalecoAmarillo <150099016+ChalecoAmarillo@users.noreply.github.com>
2024-02-25 15:52:04 -0300

    Update adsredirect.php

--- adsredirect.php
+++ adsredirect.php
@@ -1,1 +1,4 @@
-sdsd
+<?php
+header("Location: https://decimalmasculineawe.com/f2kjv1xte?key=f795f4ecc754459bd15b93bea29003d7");
+exit; // Asegura que el script se detenga después de la redirección
+?>
